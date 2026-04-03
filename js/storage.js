94a5bb61e2c132bec98144706addcf298c11303f
dimas70800 <dima.kucherenko7080@gmail.com>
2026-04-04 00:35:48 +0300

    мелкие изменения

--- js/storage.js
+++ js/storage.js
@@ -75,7 +75,7 @@ class StorageManager {
     // Сброс прогресса пользователя
     resetUserProgress(login, categoryName = null) {
         const progress = this.loadUserProgress(login);
-        
+
         if (categoryName) {
             // Сброс только по категории
             delete progress.ResolvedQuestions[categoryName];
@@ -84,9 +84,9 @@ class StorageManager {
             progress.ResolvedQuestions = {};
             progress.Score = 0;
         }
-        
+
         this.saveUserProgress(progress);
-        
+
         // Логируем сброс
         this.saveLog({
             type: 'reset',
@@ -95,7 +95,7 @@ class StorageManager {
             date: new Date().toISOString(),
             message: categoryName ? `Сброшен прогресс по категории "${categoryName}"` : 'Сброшен весь прогресс'
         });
-        
+
         return progress;
     }
 
@@ -108,28 +108,32 @@ class StorageManager {
 
         // Категория Математика - 10 вопросов
         const mathCategory = new Category("Математика");
-        for (let i = 1; i <= 10; i++) {
-            mathCategory.addQuestion(new Question(
-                `Сколько будет 2 + ${i}?`,
-                [`${2 + i}`, `${2 + i + 1}`, `${2 + i - 1}`, `${i}`],
-                0,
-                10
-            ));
-        }
+
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 1?", ["4", "5", "3", "2"], 2, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 2?", ["4", "3", "5", "6"], 0, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 3?", ["6", "5", "4", "7"], 1, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 4?", ["5", "7", "8", "6"], 3, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 5?", ["8", "6", "7", "9"], 2, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 6?", ["8", "9", "7", "10"], 0, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 7?", ["10", "9", "8", "11"], 1, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 8?", ["9", "11", "12", "10"], 3, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 9?", ["10", "12", "11", "13"], 2, 10));
+        mathCategory.addQuestion(new Question("Сколько будет 2 + 10?", ["11", "12", "13", "14"], 1, 10));
+
         categories.push(mathCategory);
 
         // Категория География - 10 вопросов
         const geographyCategory = new Category("География");
-        geographyCategory.addQuestion(new Question("Столица России?", ["Москва", "Санкт-Петербург", "Новосибирск", "Казань"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самая длинная река в мире?", ["Амазонка", "Нил", "Янцзы", "Миссисипи"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самая высокая гора?", ["Эверест", "К2", "Канченджанга", "Лхоцзе"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самое большое озеро?", ["Каспийское море", "Верхнее", "Виктория", "Гурон"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самая маленькая страна?", ["Ватикан", "Монако", "Сан-Марино", "Лихтенштейн"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самый густонаселенный город?", ["Токио", "Дели", "Шанхай", "Сан-Паулу"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самая большая пустыня?", ["Сахара", "Антарктическая", "Аравийская", "Гоби"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самый большой океан?", ["Тихий", "Атлантический", "Индийский", "Северный Ледовитый"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самая высокая точка Европы?", ["Эльбрус", "Монблан", "Дыхтау", "Казбек"], 0, 10));
-        geographyCategory.addQuestion(new Question("Самый большой материк?", ["Евразия", "Африка", "Северная Америка", "Южная Америка"], 0, 10));
+        geographyCategory.addQuestion(new Question("Столица России?", ["Санкт-Петербург", "Москва", "Казань", "Новосибирск"], 1, 10));
+        geographyCategory.addQuestion(new Question("Самая длинная река в мире?", ["Нил", "Амазонка", "Миссисипи", "Янцзы"], 1, 10));
+        geographyCategory.addQuestion(new Question("Самая высокая гора?", ["К2", "Эверест", "Лхоцзе", "Канченджанга"], 1, 10));
+        geographyCategory.addQuestion(new Question("Самое большое озеро?", ["Верхнее", "Виктория", "Гурон", "Каспийское море"], 3, 10));
+        geographyCategory.addQuestion(new Question("Самая маленькая страна?", ["Монако", "Сан-Марино", "Ватикан", "Лихтенштейн"], 2, 10));
+        geographyCategory.addQuestion(new Question("Самый густонаселенный город?", ["Дели", "Шанхай", "Сан-Паулу", "Токио"], 3, 10));
+        geographyCategory.addQuestion(new Question("Самая большая пустыня?", ["Антарктическая", "Аравийская", "Гоби", "Сахара"], 3, 10));
+        geographyCategory.addQuestion(new Question("Самый большой океан?", ["Атлантический", "Индийский", "Северный Ледовитый", "Тихий"], 3, 10));
+        geographyCategory.addQuestion(new Question("Самая высокая точка Европы?", ["Монблан", "Дыхтау", "Казбек", "Эльбрус"], 3, 10));
+        geographyCategory.addQuestion(new Question("Самый большой материк?", ["Африка", "Северная Америка", "Южная Америка", "Евразия"], 3, 10));
         categories.push(geographyCategory);
 
         // Категория История - 10 вопросов
